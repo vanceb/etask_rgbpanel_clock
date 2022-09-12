1b4ca9c280706df09cc974d2333111a703206691
vanceb <vance@axxe.co.uk>
2022-09-12 15:54:04 +0100

    First working display

--- etask_rgbpanel_clock.cpp
+++ etask_rgbpanel_clock.cpp
@@ -1,6 +1,7 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
 #include <etask_rgbpanel_clock.h>
-#include <etask_wifi.h>
-#include <Timezone.h>
 
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
@@ -30,7 +31,7 @@ void etask_rgbpanel_clock(void *parameters)
         PANEL_CHAIN, // Chain length
         _pins        // Custom pins
     );
-
+    mxconfig.double_buff = true; // Turn on double buffer
     mxconfig.clkphase = false;
     mxconfig.driver = HUB75_I2S_CFG::FM6126A;
 
@@ -39,16 +40,33 @@ void etask_rgbpanel_clock(void *parameters)
     dma_display->begin();
     dma_display->setBrightness8(90); // 0-255
     dma_display->clearScreen();
+    dma_display->flipDMABuffer();
 
     /* Run forever - Required for FreeRTOS task */
     for (;;)
     {
-        time_t t = UK.toLocal(now());
+        struct tm timeinfo;
 
-        dma_display->setTextSize(1);     // size 1 == 8 pixels high
-        dma_display->setTextWrap(false); // Don't wrap at end of line - will do ourselves
-        dma_display->setCursor(5, 0);    // start at top left, with 8 pixel of spacing
-        dma_display->printf("%2d:%2d:%2d", hour(t), minute(t), second(t))
-        delay(1000);
+        getLocalTime( &timeinfo );
+
+        dma_display->flipDMABuffer();
+        dma_display->clearScreen();
+        // Valid only if year > 2000. 
+        // You can get from timeinfo : tm_year, tm_mon, tm_mday, tm_hour, tm_min, tm_sec
+        if (timeinfo.tm_year > 100 )
+        {
+            dma_display->setTextSize(2);     // size 1 == 8 pixels high
+            dma_display->setTextWrap(false); // Don't wrap at end of line - will do ourselves
+            dma_display->setCursor(3, 9);    // start at top left, with 8 pixel of spacing
+            dma_display->printf("%02d:%02d:%02d", timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+            delay(100);
+        } else {
+            dma_display->setTextSize(1);     // size 1 == 8 pixels high
+            dma_display->setTextWrap(false); // Don't wrap at end of line - will do ourselves
+            dma_display->setCursor(0, 0);    // start at top left, with 8 pixel of spacing
+            dma_display->printf("Wifi: %s", WiFi.status() == WL_CONNECTED ? "OK" : "Wait");
+            dma_display->setCursor(0, 9);
+            dma_display->printf(WiFi.localIP().toString().c_str());
+        }
     }
 }
